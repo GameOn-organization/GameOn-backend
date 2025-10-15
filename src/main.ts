ca75460cd2a51bcac5adc97a38aead266f2bc8cd
Arthur-Queiroz <arthurdequeiroz2005@gmail.com>
2025-10-15 14:05:03 -0300

    adicionando mais ips ao cors

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,11 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
+      'http://localhost:8081',
       'http://localhost:19006',
       'http://192.168.1.100:19000',
       'exp://localhost:19000',
+      'exp://t_vdzm4-vitormimaki-8081.exp.direct',
     ],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
